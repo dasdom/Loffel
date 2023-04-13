58ac127db6266cbd6a52bb9c92349b1ff77de1f1
Dominik Hauser <dom@dasdom.de>
2023-04-13 22:39:21 +0200

    Initial Commit

---
+++ Loffel/LoffelApp.swift
@@ -0,0 +1,15 @@
+//  Created by Dominik Hauser on 13.04.23.
+//  
+//
+
+
+import SwiftUI
+
+@main
+struct LoffelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
